9d7a00e132c0b84f461c090bec88d8d4f353cdcd
Jin Kim <jinwoov@gmail.com>
2020-08-31 08:36:36 -0700

    08/31/2020 08:36:36

---
+++ server/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "hello this is jin"
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.error(`Listening at ${PORT}`);
+});
