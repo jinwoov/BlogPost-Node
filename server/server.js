13c997b70925cce97d5652e98260579033588be5
Jin Kim <jinwoov@gmail.com>
2020-08-31 09:56:10 -0700

    server and client is set work from dev branch

--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+
+
 require("dotenv").config();
+
 const app = express();
 
 app.use(helmet());
