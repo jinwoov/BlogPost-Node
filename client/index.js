4e9d472d8f7261311eeaa918816fb799f1690e67
Jin Kim <jinwoov@gmail.com>
2020-09-07 00:27:24 -0700

    able to push everything to the front end decide if i want to superagent or ajax

--- client/index.js
+++ client/index.js
@@ -3,15 +3,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const superagent = require("superagent");
 
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.static('./public'));
 
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
     res.render('index')
-})
+});
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
---
+++ client/public/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function getPosts() {
+    let url = 'http://localhost:3000/post'
+    let ajaxSettings = {
+        async: true,
+        crossDomain: true,
+        method: 'get',
+        dataType: 'json'
+    };
+    $.ajax(url, ajaxSettings)
+        .then(result => {
+            console.log(result);
+        })
+}
+
+getPosts();
+
+// able topass it to the front end 
